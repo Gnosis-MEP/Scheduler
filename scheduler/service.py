d782525979f2399f295484d3300ef813cf4e6de4
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2020-04-20 17:30:44 +0100

    very first simple version

--- scheduler/service.py
+++ scheduler/service.py
@@ -8,6 +8,7 @@ from event_service_utils.tracing.jaeger import init_tracer
 class Scheduler(BaseTracerService):
     def __init__(self,
                  service_stream_key, service_cmd_key,
+                 event_dispatcher_data_key,
                  stream_factory,
                  logging_level,
                  tracer_configs):
@@ -21,25 +22,61 @@ class Scheduler(BaseTracerService):
             tracer=tracer,
         )
         self.cmd_validation_fields = ['id', 'action']
-        self.data_validation_fields = ['id']
+        self.data_validation_fields = ['id', 'buffer_stream_key']
+        self.event_dispatcher_data = self.stream_factory.create(key=event_dispatcher_data_key, stype='streamOnly')
 
-    # def send_event_to_somewhere(self, event_data):
-    #     self.logger.debug(f'Sending event to somewhere: {event_data}')
-    #     self.write_event_with_trace(event_data, self.somewhere_stream)
+        self.bufferstream_to_dataflow = {}
+
+    def execute_adaptive_plan(self, dataflow):
+        self.bufferstream_to_dataflow = dataflow
+
+    def send_event_to_dispatcher(self, event_data):
+        self.logger.debug(f'Sending event to Event Dispather: {event_data}')
+        self.write_event_with_trace(event_data, self.event_dispatcher_data)
+
+    def apply_dataflow_to_event(self, event_data):
+        buffer_stream_key = event_data['buffer_stream_key']
+        data_flow = self.bufferstream_to_dataflow.get(buffer_stream_key, None)
+        event_data.update({
+            'data_flow': data_flow,
+            'data_path': [],
+        })
+        return event_data
 
     @timer_logger
     def process_data_event(self, event_data, json_msg):
         if not super(Scheduler, self).process_data_event(event_data, json_msg):
             return False
-        # do something here
-        pass
+        new_event_data = self.apply_dataflow_to_event(event_data)
+        self.send_event_to_dispatcher(new_event_data)
+
+    def process_data(self):
+        stream_sources_events = list(self.all_events_consumer_group.read_stream_events_list(count=1))
+        if stream_sources_events:
+            self.logger.debug(f'Processing DATA.. {stream_sources_events}')
+
+        for stream_key_bytes, event_list in stream_sources_events:
+            stream_key = stream_key_bytes
+            if type(stream_key_bytes) == bytes:
+                stream_key = stream_key_bytes.decode('utf-8')
+            for event_tuple in event_list:
+                event_id, json_msg = event_tuple
+                try:
+                    event_data = self.default_event_deserializer(json_msg)
+                    event_data.update({
+                        'buffer_stream_key': stream_key
+                    })
+                    self.process_data_event_wrapper(event_data, json_msg)
+                except Exception as e:
+                    self.logger.error(f'Error processing {json_msg}:')
+                    self.logger.exception(e)
 
     def process_action(self, action, event_data, json_msg):
         if not super(Scheduler, self).process_action(action, event_data, json_msg):
             return False
-        if action == 'someAction':
-            # do some action
-            pass
+        if action == 'executeAdaptivePlan':
+            dataflow = event_data['dataflow']
+            self.execute_adaptive_plan(dataflow)
         elif action == 'otherAction':
             # do some other action
             pass
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -8,6 +8,7 @@ from scheduler.service import Scheduler
 from scheduler.conf import (
     SERVICE_STREAM_KEY,
     SERVICE_CMD_KEY,
+    EVENT_DISPATCHER_STREAM_KEY
 )
 
 
@@ -15,6 +16,7 @@ class TestScheduler(MockedServiceStreamTestCase):
     GLOBAL_SERVICE_CONFIG = {
         'service_stream_key': SERVICE_STREAM_KEY,
         'service_cmd_key': SERVICE_CMD_KEY,
+        'event_dispatcher_data_key': EVENT_DISPATCHER_STREAM_KEY,
         'logging_level': 'ERROR',
         'tracer_configs': {'reporting_host': None, 'reporting_port': None},
     }
@@ -40,3 +42,61 @@ class TestScheduler(MockedServiceStreamTestCase):
         self.assertTrue(mocked_process_action.called)
         self.service.process_action.assert_called_once_with(action=action, event_data=event_data, json_msg=msg_tuple[1])
 
+    @patch('scheduler.service.Scheduler.execute_adaptive_plan')
+    def test_process_action_should_call_process_action_executeAdaptivePlan(self, mocked_execute_plan):
+        action = 'executeAdaptivePlan'
+        dataflow = {
+        }
+
+        event_data = {
+            'id': 1,
+            'action': action,
+            'dataflow': dataflow
+        }
+        msg_tuple = prepare_event_msg_tuple(event_data)
+        self.service.process_action(action, event_data, msg_tuple[1])
+
+        self.assertTrue(mocked_execute_plan.called)
+        mocked_execute_plan.assert_called_once_with(
+            dataflow
+        )
+
+    @patch('scheduler.service.Scheduler.apply_dataflow_to_event')
+    @patch('scheduler.service.Scheduler.send_event_to_dispatcher')
+    def test_process_data_event_should_call_send_event_to_dispatcher(self, mocked_send_dispatcher, mocked_apply_dataflow):
+        event_data = {
+            'id': 1,
+            'buffer_stream_key': 'buffer-key',
+        }
+
+        event_data_with_dataflow = event_data.copy()
+        event_data_with_dataflow.update({
+            'data_flow': ['service1-stream', 'service2-stream'],
+            'data_path': []
+        })
+
+        mocked_apply_dataflow.return_value = event_data_with_dataflow
+        msg_tuple = prepare_event_msg_tuple(event_data)
+
+        self.service.process_data_event(event_data, msg_tuple[1])
+
+        self.assertTrue(mocked_send_dispatcher.called)
+        mocked_send_dispatcher.assert_called_once_with(
+            event_data_with_dataflow
+        )
+
+    def test_apply_dataflow_to_event_should_add_correct_fields(self):
+        event_data = {
+            'id': 1,
+            'buffer_stream_key': 'buffer-stream-key1',
+        }
+        self.service.bufferstream_to_dataflow = {
+            'buffer-stream-key1': ['service-stream1', 'service-stream2'],
+            'buffer-stream-key2': ['service-stream3', 'service-stream4'],
+        }
+
+        altered_event = self.service.apply_dataflow_to_event(event_data)
+        self.assertIn('data_flow', altered_event)
+        self.assertIn('data_path', altered_event)
+        self.assertEquals(['service-stream1', 'service-stream2'], altered_event['data_flow'])
+        self.assertEquals([], altered_event['data_path'])
